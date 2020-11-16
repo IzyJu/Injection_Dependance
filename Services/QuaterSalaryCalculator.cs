7bd685e704d4deb42c299c1deb54a7d467f6773a
Julien.Iz <69205008+IzyJu@users.noreply.github.com>
2020-11-16 16:12:18 +0100

    Add files via upload

---
+++ Services/QuaterSalaryCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    public class QuaterSalaryCalculator : ISalaryCalculator
+    {
+        public Double CalculateSalary(Double yearlyAmount)
+        {
+            return yearlyAmount / 4;
+        }
+    }
+}
